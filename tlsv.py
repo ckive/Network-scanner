d83df14b52caa213fd4ee2880604e2f555299035
ckive <danyang2023@u.northwestern.edu>
2021-03-15 01:54:51 +0800

    one more

---
+++ tlsv.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+def tls_scanner(target: str) -> list:
+    tls_v = []
+
+    url = target + ":443"
+    pro = {
+        '-tls1': "TLSv1.0",
+        '-tls1_1': "TLSv1.1",
+        '-tls1_2': "TLSv1.2",
+        '-tls1_3': "TLSv1.3",
+    }
+    try:
+        for proto in pro.keys():
+            pipein = subprocess.run(["echo"], check=True, stdout=subprocess.PIPE)
+            outcome = subprocess.run(['openssl', 's_client', proto, '-connect', str(url)],
+                                        input=pipein.stdout, stdout=subprocess.PIPE, timeout=2)
+
+            for row in outcome.stdout.decode().split('\n'):
+                row = row.lstrip()
+                if row.startswith("Server certificate"): # row we want
+                    #supports
+                    tls_v.append(pro[proto])
+    except Exception as e:
+        print("Exception")
+    
+    return tls_v
+
+tls_scanner("stevetarzia.com")
